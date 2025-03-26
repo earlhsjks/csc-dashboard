5decec457e402f5476d9cb2f60ffbb84bfaf27d4
Earl Jeno Garcia <68950014+earlhsjks@users.noreply.github.com>
2025-03-26 12:09:56 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/dtr')
+def dtr():
+    return redirect('http://localhost:5001/')  # Redirect to DTR System
+
+@app.route('/logs')
+def logs():
+    return redirect('http://localhost:5003/')  # Redirect to Logging System
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+if __name__ == '__main__':
+    # serve(app, host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
