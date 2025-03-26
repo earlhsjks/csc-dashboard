fe98d402390562ecca9475eca8dde1e004aac706
Earl Jeno Garcia <68950014+earlhsjks@users.noreply.github.com>
2025-03-26 12:10:32 +0800

    production host

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
 
 if __name__ == '__main__':
-    # serve(app, host='0.0.0.0', port=80)
-    app.run(host='0.0.0.0', port=80, debug=True)
+    serve(app, host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80, debug=True)
